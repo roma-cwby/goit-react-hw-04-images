e8ad9a14711a4a82fbdeebce811b96036a301867
Roma Antoliak <104556918+roma-cwby@users.noreply.github.com>
2022-12-19 14:50:39 +0200

    fixed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,4 @@
-import { useRef, useEffect } from 'react';
-
 export const Modal = ({ closeModal, children }) => {
-  const overlay = useRef();
-
   const closeClick = e => {
     if (e.target === e.currentTarget) close();
   };
@@ -12,18 +8,14 @@ export const Modal = ({ closeModal, children }) => {
   };
 
   const close = () => {
-    overlay.current.removeEventListener('click', closeClick);
     window.removeEventListener('keydown', closeEsc);
     closeModal();
   };
 
-  useEffect(() => {
-    overlay.current.addEventListener('click', closeClick);
-    window.addEventListener('keydown', closeEsc);
-  }, [closeClick, closeEsc]);
+  window.addEventListener('keydown', closeEsc);
 
   return (
-    <div ref={overlay} className="Overlay">
+    <div onClick={closeClick} className="Overlay">
       <div className="Modal">{children}</div>
     </div>
   );
